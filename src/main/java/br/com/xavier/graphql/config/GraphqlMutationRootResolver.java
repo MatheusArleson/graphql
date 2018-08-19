57a84bd2991c6802736ef64531c9421608eae311
Matheus Xavier <matheusarleson@gmail.com>
2018-08-19 22:46:23 +0200

    DEV Mutations - Initial Impl
    - Add mutation posts/authors

--- src/main/java/br/com/xavier/graphql/config/GraphqlMutationRootResolver.java
+++ src/main/java/br/com/xavier/graphql/config/GraphqlMutationRootResolver.java
@@ -1,9 +1,30 @@
 package br.com.xavier.graphql.config;
 
+import br.com.xavier.graphql.nodes.author.Author;
+import br.com.xavier.graphql.nodes.author.AuthorService;
+import br.com.xavier.graphql.nodes.post.Post;
+import br.com.xavier.graphql.nodes.post.PostService;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 
 public class GraphqlMutationRootResolver implements GraphQLMutationResolver {
 
+	private final PostService postService = PostService.getInstance();
+	private final AuthorService authorService = AuthorService.getInstance();
+
 	public GraphqlMutationRootResolver() {
+	}
+
+	public Post createPost(String content, Long authorId){
+		Post newPost = Post.of(postService.getNextId(), content, authorId);
+
+		postService.save(newPost);
+		return newPost;
+	}
+
+	public Author createAuthor(String name){
+		Author newAuthor = Author.of(authorService.getNextId(), name);
+
+		authorService.save(newAuthor);
+		return newAuthor;
 	}
 }
